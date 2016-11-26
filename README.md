372b4d160459eba1fe2755dc8373a3cb7f8e07ee
Thomas Gallin <thomas.gallin@gmail.com>
2016-11-26 00:12:13 +0000

    implemented routes and logic

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ I can pass a URL as a parameter and I will receive a shortened URL in the JSON r
 When I visit that shortened URL, it will redirect me to my original link.
 
 Example creation usage:
-https://urlshortener-tgallin.herokuapp.com/https://www.google.com
+https://urlshortener-tgallin.herokuapp.com/shorten/https://www.google.com
 will output
-{ "original_url":"https://www.google.com", "short_url":"https://urlshortener-tgallin.herokuapp.com/1234" }
+{ "shortened_url":"1234" }
 
 Usage:
 https://urlshortener-tgallin.herokuapp.com/1234
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var db = require('./lib/mongodb.js');
 
 var app = express();
 
@@ -6,6 +7,59 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.use(express.static('public'));
+
+function sendShortUrl(err, req, res, shortUrl) {
+    if (err) {
+        res.end('there was a problem trying to shorten the url. ' + err);
+    }
+    else {
+        var url = {
+            shortened_url: req.get('x-forwarded-proto') + '://' + req.get('host') + '/' + shortUrl
+        };
+        res.json(url);
+    }
+}
+
+app.get('/shorten/:prefix//:originalUrl', function(req, res) {
+    db.getShortUrl(req, res, sendShortUrl);
+});
+
+app.get('/shorten/:originalUrl', function(req, res) {
+    db.getShortUrl(req, res, sendShortUrl);
+});
+
+app.get('/reset', function(req, res) {
+    db.reset();
+    res.render('index', {
+        title: 'Url shortener microservice',
+        message: 'Url shortener microservice'
+    });
+});
+
+app.get('/:prefix//:shortUrl', function(req, res) {
+    db.getOriginalUrl(req, res, redirect);
+});
+
+app.get('/:shortUrl', function(req, res) {
+    db.getOriginalUrl(req, res, redirect);
+});
+
+function redirect(err, res, originalUrl) {
+    if (err) {
+        res.end('there was a problem trying to find the original url. ' + err);
+    }
+    else {
+        if (originalUrl && originalUrl.length > 0) {
+            if (!originalUrl.includes('http')) {
+                originalUrl = "http://" + originalUrl;
+            }
+            res.redirect(originalUrl);
+        }
+        else {
+            res.end('We could not find any url matching the shorten url you provided.');
+        }
+    }
+}
 
 app.get('/', function(req, res) {
     res.render('index', {
